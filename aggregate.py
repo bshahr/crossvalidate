ae9c909469c4e2e73515416a6c871c2334f98d8f
Bobak Shahriari <bshahr@cs.ubc.ca>
2012-08-02 16:18:44 -0700

    Added  to retrieve all the data from  files and to create an  file containing {'accs', 'wall'}.

--- aggregate.py
+++ aggregate.py
@@ -18,25 +18,6 @@ from time import time
 #RESULTSPATH = os.environ['PYCROSSVALIDATE_RESULTSPATH'] or 'results'
 RESULTSPATH = 'results'
 
-def run_method(method, train, test, X, y, directory, pass_to_hash):
-    """
-    Train the `method` on X[train] and test on X[test].
-    `pass_to_hash` is a tuple that is passed to joblib's
-    hasing function.
-    """
-    clf = run_delayed(method)
-
-    start = time()
-    clf.fit(X[train], y[train])
-    accs = clf.score(X[test], y[test])
-    wall = time() - start
-
-    fname = joblib.hashing.hash(pass_to_hash)
-    fname = os.path.join(directory, fname + '.pkl')
-    with open(fname, 'wb-') as pklfile:
-        pickle.dump((accs, wall), pklfile)
-
-
 import sklearn.svm
 import sklearn.naive_bayes
 import sklearn.ensemble
@@ -86,25 +67,14 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('dataset', type=file, help='dataset to use')
-    parser.add_argument('--n-jobs', dest='n_jobs', type=int,
-                        help='number of total jobs to run', default=0)
-    parser.add_argument('-j', type=int, help='job id to run')
     parser.add_argument('-n', type=int, help='number of datapoints to use', default=0)
     parser.add_argument('-k', type=int, help='number of folds', default=0)
     args = parser.parse_args()
 
     # Load data
     X = np.load(args.dataset)
-    y = X[:,-1]
-    X = X[:,:-1]
-
     n = args.n if args.n else len(X)
     M = len(methods)
-    J = args.k * M if args.k else n * M
-    n_jobs = min(J, args.n_jobs) if args.n_jobs else J
-
-    if args.j < 0 or args.j >= n_jobs:
-        raise ValueError('Job ID out of range.')
 
     # Create results directory
     datafname = args.dataset.name.split('/')[-1]        # remove data directory
@@ -114,28 +84,33 @@ if __name__ == '__main__':
                 )
     directory = os.path.join(RESULTSPATH, base)
     try:
-        os.mkdir(directory)
+        os.chdir(directory)
     except OSError:
-        pass
-
-    # Range of jobs to run
-    job_batchsize = int(J / n_jobs)
-    a = args.j * job_batchsize
-    b = a + job_batchsize if (args.j < n_jobs-1) else J
+        print 'Cannot find dataset {0:s} with k={1:02d}.'.format(args.dataset,
+                                                                 args.k)
 
     # Get cross-validation folds
     cv = sl.cross_validation.LeaveOneOut(n) if (args.k == 0) else \
          sl.cross_validation.KFold(n, args.k)
 
     # Setup list of jobs
-    jobs = iter(delayed(run_method)(method, train, test, X, y,
-                                    directory=directory,
-                                    pass_to_hash=(args.dataset, method, n,
-                                                  args.k, train, test))
-                for method, (train, test) in itertools.product(methods, cv))
+    files = iter(joblib.hashing.hash((args.dataset, method, n, args.k, train, test))
+                 + '.pkl'
+                 for method, (train, test) in itertools.product(methods, cv))
 
-    # Run only jobs in batch
-    for job in itertools.islice(jobs, a, b):
-        run_delayed(job)
+    # Open pickles and assign to matrices
+    accs = np.empty(M * args.k)
+    wall = np.empty(M * args.k)
+    accs.fill(np.nan)       # if the file does not exist set to np.nan
+    wall.fill(np.nan)
+    for i, file_ in enumerate(files):
+        try:
+            with open(file_, 'rb') as pklfile:
+                accs[i], wall[i] = pickle.load(pklfile)
+        except IOError:
+            pass
 
+    accs.resize((M, args.k))
+    wall.resize((M, args.k))
 
+    np.savez(base, accs=accs, wall=wall)
