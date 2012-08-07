caea0685c3f1c5cf66a16b126de032477141fe10
Bobak Shahriari <bshahr@cs.ubc.ca>
2012-08-07 15:54:38 -0700

    Added GBC to methods, and removed extremely slow hyper-parameter configs for SVM(poly3)

--- methods_list.py
+++ methods_list.py
@@ -18,11 +18,13 @@ def make_methods_list():
     methods = []
 
     # SVMs.
-    for C in [1e3, 1e4, 1e5]:
+    for C in [1e2, 1e3, 1e4]:
         for gamma in [0.01, 0.05, 0.1]:
             methods.append(delayed(sl.svm.SVC)(kernel='rbf', C=C, gamma=gamma))
-            methods.append(delayed(sl.svm.SVC)(kernel='poly', degree=2, C=C, gamma=gamma))
-            methods.append(delayed(sl.svm.SVC)(kernel='poly', degree=3, C=C, gamma=gamma))
+            methods.append(delayed(sl.svm.SVC)(kernel='poly', degree=2, C=C,
+                                               gamma=gamma))
+            methods.append(delayed(sl.svm.SVC)(kernel='poly', degree=3, C=C,
+                                               gamma=gamma))
         methods.append(delayed(sl.svm.SVC)(kernel='linear', C=C))
 
     # Misc.
@@ -31,30 +33,37 @@ def make_methods_list():
 
     # Random Forests.
     for n in [10, 50, 100, 200, 500]:
-        methods.append(delayed(sl.ensemble.RandomForestClassifier)(n_estimators=n))
+        methods.append(delayed(sl.ensemble.RandomForestClassifier)
+                              (n_estimators=n))
 
     for n in [10, 50, 100, 200, 500]:
-        methods.append(delayed(sl.ensemble.RandomForestClassifier)(n_estimators=n, criterion='entropy'))
+        methods.append(delayed(sl.ensemble.RandomForestClassifier)
+                              (n_estimators=n, criterion='entropy'))
 
     # Gradient boosting.
-#    for n in [10, 50, 100, 200, 500]:
-#        for a in [.0001, .001, .01, .1, 1., 10.]:
-#            methods.append(delayed(sl.ensemble.GradientBoostingClassifier)(n_estimators=n, learn_rate=a))
+    for n in [10, 50, 100, 200, 500]:
+        for a in [.0001, .001, .01, .1, 1., 10.]:
+            methods.append(delayed(sl.ensemble.GradientBoostingClassifier)
+                                  (n_estimators=n, learn_rate=a))
 
     # Nearest neighbors.
     for n in [1, 5, 10, 15, 20]:
-        methods.append(delayed(sl.neighbors.KNeighborsClassifier)(n_neighbors=n))
+        methods.append(delayed(sl.neighbors.KNeighborsClassifier)
+                              (n_neighbors=n))
 
     for n in [1, 5, 10, 15, 20]:
-        methods.append(delayed(sl.neighbors.KNeighborsClassifier)(n_neighbors=n, weights='distance'))
+        methods.append(delayed(sl.neighbors.KNeighborsClassifier)
+                              (n_neighbors=n, weights='distance'))
 
     # l1-penalized logistic regression
     for c in [0.0001, 0.001, 0.01, 0.05, 0.1, 0.5, 1, 5, 10, 50, 100]:
-        methods.append(delayed(sl.linear_model.LogisticRegression)(C=c, penalty='l1', tol=0.01))
+        methods.append(delayed(sl.linear_model.LogisticRegression)
+                              (C=c, penalty='l1', tol=0.01))
 
     # l2-penalized logistic regression
     for c in [0.0001, 0.001, 0.01, 0.05, 0.1, 0.5, 1, 5, 10, 50, 100]:
-        methods.append(delayed(sl.linear_model.LogisticRegression)(C=c, penalty='l2', tol=0.01))
+        methods.append(delayed(sl.linear_model.LogisticRegression)
+                              (C=c, penalty='l2', tol=0.01))
 
 
     return methods
